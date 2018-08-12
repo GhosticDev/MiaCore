fc673150edd39885e4f36ddabe100c4c628fc0ae
GhosticDev <ghosticman@gmail.com>
2018-08-12 12:59:50 +0100

    Update index.js

--- index.js
+++ index.js
@@ -17,10 +17,14 @@ client.on("message", async message => {
 	if (message.content.indexOf(config.prefix) !== 0) return;
 	const args = message.content.slice(config.prefix.length).trim().split(/ +/g);
 	const command = args.shift().toLowerCase();
-
   try {
+    
+    let ops = {
+      active: active
+    }
+    
     let commands = require(`./commands/${command}.js`);
-    commands.run(client, message, args);
+    await commands.run(client, message, args, ops);
   } catch (e) {
     utils.error(client, e, message.author.username);
   } finally {
